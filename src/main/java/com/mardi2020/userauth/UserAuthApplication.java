cdcc67a3391e9b6fa921851ffcbcba4c6d571603
mardi2020 <mardi@khu.ac.kr>
2022-12-07 14:08:09 +0900

    initial commit

---
+++ src/main/java/com/mardi2020/userauth/UserAuthApplication.java
@@ -0,0 +1,13 @@
+package com.mardi2020.userauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthApplication.class, args);
+    }
+
+}
