4f818f2c54370be593aa6cc535000006a9f37efb
mardi2020 <mardi@khu.ac.kr>
2022-12-10 16:41:13 +0900

    feat: aggregate swagger

---
+++ apiGateway/src/main/java/com/mardi202/apigateway/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.mardi202.apigateway.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+@Primary
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfig implements SwaggerResourcesProvider {
+
+    private final RouteLocator routeLocator;
+
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+
+        routeLocator.getRoutes().subscribe(route -> {
+            String name = route.getId();
+            resources.add(swaggerResource(name, "/" + name.toLowerCase() + "/v3/api-docs", "1.0"));
+        });
+
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(final String name, final String location,
+                                            final String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+}
