d71e0d1d76d770c6b26ccefa492cddd61603d59d
mardi2020 <mardi@khu.ac.kr>
2022-12-12 21:48:53 +0900

    feat: gateway token 검증 추가

---
+++ apiGateway/src/main/java/com/mardi202/apigateway/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,74 @@
+package com.mardi202.apigateway.filter;
+
+import io.jsonwebtoken.Jwts;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    private final Environment env;
+
+    public AuthorizationHeaderFilter(Environment env) {
+        super(Config.class);
+        this.env = env;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            String authorizationHeader = Objects
+                    .requireNonNull(request.getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
+            String token = authorizationHeader.replace("Bearer", "");
+
+            if (!isJwtValid(token)) {
+                return onError(exchange);
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        log.error("JWT not valid");
+        return response.setComplete();
+    }
+
+    private boolean isJwtValid(String token) {
+        boolean result = true;
+        String subject = null;
+        try {
+            subject = Jwts.parser()
+                    .setSigningKey(env.getProperty("token.secret"))
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+        } catch (Exception e) {
+            result = false;
+        }
+        if (subject == null || subject.isEmpty()) {
+            result = false;
+        }
+        return result;
+    }
+
+    public static class Config {
+
+    }
+}
